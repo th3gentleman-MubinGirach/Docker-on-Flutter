16b4e563ca9305e89d7f58606df657abfc473c3e
th3gentleman-MubinGirach <girachmubin@gmail.com>
2021-08-25 21:21:39 +0530

    Delete docker directory

--- docker/android/app/src/main/kotlin/com/example/DockerApp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.DockerApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- docker/android/app/src/main/kotlin/com/example/docker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.docker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
